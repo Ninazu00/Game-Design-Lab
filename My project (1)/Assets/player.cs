eaba21a6263d9083c6e0dc724451ba1248111b1e
Ninazu00 <ba.mohammad2005@gmail.com>
2025-10-21 10:54:05 +0300

    CommitLab

--- My project (1)/Assets/player.cs
+++ My project (1)/Assets/player.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class movement : MonoBehaviour
+public class player : MonoBehaviour
 {
     // Start is called before the first frame update
     public float moveSpeed;
@@ -14,37 +14,45 @@ public class movement : MonoBehaviour
     public float GCRadius;
     public LayerMask whatIsGround;
     private bool grounded;
+    private Animator anim;
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(L)){
+        if (Input.GetKey(L))
+        {
             GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
-            if (GetComponent<SpriteRenderer>()!=null){
+            if (GetComponent<SpriteRenderer>() != null)
+            {
                 GetComponent<SpriteRenderer>().flipX = true;
             }
         }
-        if(Input.GetKey(R)){
+        if (Input.GetKey(R))
+        {
             GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-            if (GetComponent<SpriteRenderer>()!=null){
+            if (GetComponent<SpriteRenderer>() != null)
+            {
                 GetComponent<SpriteRenderer>().flipX = false;
             }
         }
-        if(Input.GetKeyUp(spacebar)&& grounded){
+        if (Input.GetKeyUp(spacebar) && grounded)
+        {
             Jump();
         }
+        anim.SetFloat("Speed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+        anim.SetFloat("VSpeed", GetComponent<Rigidbody2D>().velocity.y);
+        anim.SetBool("Grounded", grounded);
     }
-
+    
     void FixedUpdate(){
         grounded = Physics2D.OverlapCircle(groundCheck.position, GCRadius, whatIsGround);
     }
-
-
+    
     void Jump(){
         GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
     }
